8b5ec86cbc8df3d7827e3bbf62932f79cd135a76
Lucas Mendonça <lucas_lam@live.com>
2019-12-08 02:47:18 -0300

    Error lol

--- web_service/employees_new_action.php
+++ web_service/employees_new_action.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-$include ('sql_connect.php');
+include ('sql_connect.php');
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 
