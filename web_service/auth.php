cb3ef4ff4dc9c72d1fd71447080ce308ea702015
Lucas Mendonça <lucas_lam@live.com>
2019-12-08 01:47:34 -0300

    FIXED Query

--- web_service/auth.php
+++ web_service/auth.php
@@ -9,23 +9,11 @@ session_start();
 
 include('sql_connect.php');
 
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-
-echo "<br>";
-
 
 $email = addslashes($_POST['email']);
 $password = addslashes($_POST['password']);
 
-$sql = "SELECT * FROM `Users` WHERE email LIKE '$email'";
+$sql = "SELECT * FROM `User` WHERE email LIKE '$email'";
 echo $sql;
 	if ($res = mysqli_query($link, $sql)) {
 	    if(mysqli_num_rows($res) > 0){
