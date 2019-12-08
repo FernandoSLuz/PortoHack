1d680eab6013136e66ed986c217258fb2279e94b
Lucas Mendonça <lucas_lam@live.com>
2019-12-08 10:05:11 -0300

    no message

--- web_service/dashboard.php
+++ web_service/dashboard.php
@@ -39,10 +39,44 @@ ini_set('display_startup_errors', 1);
     else{
       echo "Erro de banco";
     }
-
    ?>
    <br><br>
    <div id="map" style="" >
+     <?php
+       include('sql_connect.php');
+
+       $sql = "SELECT * FROM Report";
+
+       if($res = mysqli_query($link, $sql)){
+         if(mysqli_num_rows($res)>0){
+           while($row = mysqli_fetch_array($res)){
+             ?>
+               <style>
+                  #request2{
+                    display: block;
+                    position: relative;
+                    top: <?php echo $row['y']?>px;
+                    left: <?php echo $row['x']?>px;
+                    height: 20px; width: 20px;
+                    border-radius: 10px;
+                    border: solid 2px #000;
+                    background-color: #3c3;
+                  }
+               </style>
+               <div id="request2">
+                  <a href="#">&nbsp;</a>
+               </div>
+             <?php
+           }
+         }
+         else{
+           echo "Não temos solicitações";
+         }
+       }
+       else{
+         echo "Erro de banco";
+       }
+      ?>
      <style>
         #request1{
           display: block;
