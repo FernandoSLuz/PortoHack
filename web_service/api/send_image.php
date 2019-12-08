7809a711f2c6fdedb47a7a29e4e634a54c7f8bc4
Lucas Mendonça <lucas_lam@live.com>
2019-12-08 10:55:37 -0300

    no message

---
+++ web_service/api/send_image.php
@@ -0,0 +1,26 @@
+<?php
+if(!isset($_REQUEST['image']) || !isset($_REQUEST['id_user']) || !isset($_REQUEST['name'])){
+    echo "Campos n達o preenchidos corretamente.";
+}
+else{
+    echo "id : ".$_REQUEST['id_user']." - image: ".$_REQUEST['image'];
+    define('UPLOAD_DIR', 'uploadedimages/');
+    $image_parts = explode(";base64,", 'data:image/jpeg;base64,'.$_REQUEST['image']);
+    $image_type_aux = explode("image/", $image_parts[0]);
+    $image_type = $image_type_aux[1];
+    $image_base64 = base64_decode($image_parts[1]);
+    $image_name = uniqid();
+    $file = UPLOAD_DIR . $image_name . '.jpg';
+    file_put_contents($file, $image_base64);
+    include_once("../sqlconnect.php");
+
+    $sql = "INSERT INTO tkd_dii_images(id, user_id, image_path, name, timestamp) VALUES(null, '".$_REQUEST['id_user']."','".$image_name."','".$_REQUEST['name']."', null)";
+
+    if(mysqli_query($link, $sql)){
+      echo " - Cadastrado com sucesso";
+    }
+    else{
+      echo " - Erro de conex達o com o baco";
+    }
+}
+?>
