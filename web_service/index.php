1de76e3b5542f8d4f561ed7e8373f489165a8555
Lucas Mendonça <lucas_lam@live.com>
2019-12-08 01:23:06 -0300

    Bootstrap fix

--- web_service/index.php
+++ web_service/index.php
@@ -25,6 +25,7 @@ mysqli_close($link);
   <head>
     <title><?php echo $appName?></title>
     <link rel="stylesheet" type="text/css" href="@{'/public/stylesheets/datatables-bootstrap.css'}"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <style>
       body{
         padding: 30px;
