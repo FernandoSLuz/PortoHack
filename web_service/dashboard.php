061fa96341a186681e2d5b69c71e3811e8788a1c
Lucas Mendonça <lucas_lam@live.com>
2019-12-08 02:13:56 -0300

    Dashboard backend response

--- web_service/dashboard.php
+++ web_service/dashboard.php
@@ -1,4 +1,22 @@
 <?php include('main/header.php');?>
-Dashboard
+<h1>Dashboard</h1>
 
+  <br><a href="employees.php">Funcionários</a>
+  <br><a href="logout.php">Logout</a>
+
+  <br><br>
+  <?php
+    include('sql_connect.php');
+    $sql = "SELECT * FROM Report";
+    if($res = mysqli($link, $sql)){
+      if(mysqli_num_rows($res)>0){
+        while($row = mysqli_fetch_array($res)){
+          echo "<br>".$row['description'];
+        }
+      }
+    }
+    else{
+      echo "Não temos solicitações";
+    }
+   ?>
 <?php include('main/footer.php');?>
